10dda2559a088ed1324d87143d6325b804de88d0
Caleb Brazeau <cjbrazeau12@gmail.com>
2022-08-29 21:29:49 -0400

    Using Tags

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -6,6 +6,13 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other)
     {
-        GetComponent<MeshRenderer>().material.color = Color.magenta;
+        // If the collided object has the "Player" tag
+        if(other.gameObject.tag == "Player")
+        {
+            // Change objects Mesh Renderer to specified color
+            GetComponent<MeshRenderer>().material.color = Color.magenta;
+            // Change objects tag to "Hit"
+            gameObject.tag = "Hit";
+        }
     }
 }
--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Scorer : MonoBehaviour
 {
+    // Variable to store the number of hits
     private int intHits = 0;
 
     private void OnCollisionEnter(Collision other) {
-        intHits++;
-        Debug.Log("You have bumped into something : " + intHits + " Times");
+        // If the collided object does not have the "Hit" tag
+        if(other.gameObject.tag != "Hit")
+        {
+            // Increment the number of hits variable
+            intHits++;
+            // Log the number of hits
+            Debug.Log("You have bumped into something : " + intHits + " Times");
+        }
     }
 }
