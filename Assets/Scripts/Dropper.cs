6f345c75a94be50bf5529ec187e9ee85677dc7f4
Caleb Brazeau <cjbrazeau12@gmail.com>
2022-08-29 21:19:40 -0400

    Caching A Reference

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    MeshRenderer renderer;
+    Rigidbody rigidbody;
     [SerializeField] float fltTimeToWait = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get objects Mesh Renderer
+        renderer = GetComponent<MeshRenderer>();
+        // Disable objects Mesh Renderer
+        renderer.enabled = false;
+
+        // Get objects Rigidbody
+        rigidbody = GetComponent<Rigidbody>();
+        // Disable objects Rigidbody gravity
+        rigidbody.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Time elapsed since game started: " + Time.time);
+        // If a certain amount of time has passed
         if(Time.time > fltTimeToWait)
         {
-            Debug.Log("Do something");
+            // Enable object mesh renderer and gravity
+            renderer.enabled = true;
+            rigidbody.useGravity = true;
         }
     }
 }
